4a43e34e3ad9fe05ac0b4e8c7cda00e235c383d8
larahollik <lara.hollik@edu.campus02.at>
2023-04-05 21:17:12 +0200

    added user and role checks for admin and main view

--- angular-frontend/src/app/view-admin/view-admin.component.ts
+++ angular-frontend/src/app/view-admin/view-admin.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {AppComponent} from "../app.component";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-view-admin',
@@ -7,7 +8,10 @@ import {AppComponent} from "../app.component";
   styleUrls: ['./view-admin.component.scss']
 })
 export class ViewAdminComponent {
-  constructor() {
+  constructor(private router: Router ) {
+    if(!AppComponent.currentUser || AppComponent.currentUser.role !== 'admin') {
+      this.router.navigate(['/start']);
+    }
   }
 
   currentUser(){
--- angular-frontend/src/app/view-main/view-main.component.ts
+++ angular-frontend/src/app/view-main/view-main.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {AppComponent} from "../app.component";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-view-main',
@@ -7,7 +8,10 @@ import {AppComponent} from "../app.component";
   styleUrls: ['./view-main.component.scss']
 })
 export class ViewMainComponent {
-  constructor() {
+  constructor(private router: Router ) {
+    if(!AppComponent.currentUser) {
+      this.router.navigate(['/start']);
+    }
   }
 
   currentUser(){
